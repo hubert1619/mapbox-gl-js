6ef26dd0a21e077df9751ff6569c8ee7c94b6d7f
Anand Thakker <github@anandthakker.net>
2018-02-12 15:13:46 -0500

    Manual es6 module fixes

--- src/source/video_source.js
+++ src/source/video_source.js
@@ -1,6 +1,6 @@
 // @flow
 
-import ajax from '../util/ajax';
+import { getVideo } from '../util/ajax';
 
 import ImageSource from './image_source';
 import rasterBoundsAttributes from '../data/raster_bounds_attributes';
@@ -64,7 +64,7 @@ class VideoSource extends ImageSource {
         const options = this.options;
         this.urls = options.urls;
 
-        ajax.getVideo(options.urls, (err, video) => {
+        getVideo(options.urls, (err, video) => {
             if (err) {
                 this.fire(new ErrorEvent(err));
             } else if (video) {
